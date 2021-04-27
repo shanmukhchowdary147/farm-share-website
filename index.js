86fea1ea79e9571abfb18de5e941263228d5fafe
shanmukhchowdary147 <shanmukhchowdary147@gmail.com>
2021-04-27 20:02:52 +0530

    private information secured by moving to dev.js

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://shannu:123456Aa@farm-share.an27a.mongodb.net/farm-share?retryWrites=true&w=majority',
-        {useNewUrlParser: true}).then(()=> console.log('Connected'))
-                                .catch(err=> console.error(err));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key')
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
+        {useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,}
+                ).then(()=> console.log('Connected'))
+                .catch(err=> console.error(err));
 
 
-app.get('/', (req,res)=>{
-    res.send('Shannu')
-});
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.get('/', (req,res)=> {
+    res.json({"Hello ":"Shannu u r a really changed man"})
+})
+
+app.post('/api/user/register',(req,res) => {
+        const user = new User(req.body)
+
+        user.save((err, userData)=> {
+            if(err) return res.json ({ success: false, err})
+            return res.status(200).json({
+                success:true
+            })
+        })
+        
+        
+})
+
+
 
 
 app.listen(5000);
